662ad2b74ae82849d185d598743cd7db23f42cb7
OlexiiKostin84 <129264896+OlexiiKostin84@users.noreply.github.com>
2023-06-06 21:02:53 +0300

    1

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './js/slick';
+import './js/mobile-menu';
+import './js/modal-hero';
+import './js/modal-sellers';
+// import './js/modal';
+import './js/slick-reviews';
+import './js/modal-reviews';
+import './js/modal-subscribe';
